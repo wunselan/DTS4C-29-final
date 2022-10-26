5a977e1855bd5701f25cd07c94ba88e98f09b902
Wunsel <wunselan@gmail.com>
2022-10-26 23:26:17 +0700

    comment provider

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import store from './redux/redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
     <App />
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>
 );
 
